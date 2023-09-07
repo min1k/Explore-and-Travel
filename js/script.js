3ee77948bee96455c049f07da9f1dc5f63336e71
Masyuk <masyuk0tanika@gmail.com>
2023-09-07 16:19:05 +0300

    Add your commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use strict" //директива
+
+document.addEventListener('click', documentClick);
+
+function documentClick(e) {
+	const targetItem = e.target;
+
+	if (targetItem.closest('.icon-menu')){
+		document.documentElement.classList.toggle('menu-open');
+	}
+}
+
